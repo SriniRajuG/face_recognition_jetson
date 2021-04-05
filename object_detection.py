b24997cae85c96412164901e694b8d50b6f07610
srini <srini.gottimukkala@arcadium.ai>
2021-04-05 12:45:41 -0400

    Add a function to encode training images.

--- object_detection.py
+++ object_detection.py
@@ -1,8 +1,12 @@
 # img_mat: image matrix
 
+import os
+from pathlib import Path
+import pickle
 import sys
 
 import cv2 as cv
+import face_recognition as fr
 import numpy as np
 
 
@@ -36,10 +40,33 @@ def display_video_from_cam() -> None:
     cv.destroyAllWindows()
 
 
+def create_face_encoding():
+    """
+    - Load images from a directory with training images.
+    - Generate encoding for each face.
+    - Persist the encoding and the name of the person as a pickle file.
+    - Each training image has a single human face.
+    - Name of the file is the name of the person in the image.
+    """
+    img_file_extns = ('.jpeg', '.jpg', '.png', '.gif', '.tiff')
+    encodings_dir_path = Path('./data/train_faces/face_encodings')
+    encodings_dir_path.mkdir(exist_ok=True)
+    with os.scandir("./data/train_faces") as dir:
+        for entry in dir:
+            if entry.is_file() and entry.name.lower().endswith(img_file_extns):
+                face_name = os.path.splitext(entry.name)[0]
+                img_mat = fr.load_image_file(entry.path)
+                face_encoding = fr.face_encodings(img_mat)[0]
+                out_filepath = encodings_dir_path / f'{face_name}.pkl'
+                with open(out_filepath, 'wb') as f:
+                    pickle.dump(face_encoding, f)
+
+
 def main():
     # img_mat = read_img_from_file(file_name='./data/puppy.jpg')
     # display_img(img_mat, delay_in_millsec=4000)
-    display_video_from_cam()
+    # display_video_from_cam()
+    create_face_encoding()
 
 
 if __name__ == '__main__':
